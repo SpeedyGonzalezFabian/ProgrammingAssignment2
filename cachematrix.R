fd70c357d56bf0202fa0ac16d4992a8c1dee999a
SpeedyGonzalezFabian <sisac01@MN080118-C0016.sle4.uni-tuebingen.de>
2015-11-19 10:07:42 +0100

    Solution to Programming Assignment 2
    Two functions to create a special matrix that can cache its inverse and
    to retrieve this inverse, if it has been cached already, or calculate it
    and cache it

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,74 @@
-## Put comments here that give an overall description of what your
-## functions do
+##Two functions to create a special matrix object that can cache its inverse (makeCacheMatrix)
+##and that retrieve the inverse of a matrix if it has been cached already or calculate it and then cache it (solveCache)
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+##function creating a special matrix that can cache its inverse
+##note: this also works of "a" is a numeric vector that can be turned into an invertible matrix
+##input: a = matrix or numeric vector; rows = number of rows and columns of matrix, if a is vector
 
+makeCacheMatrix <- function(a ,rows = 2) {
+        #set "inv" to NULL, because new matrix was created
+        inv <- NULL
+        
+        #check whether matrix or vector was given
+        if (is.matrix(a)){
+                x<-a
+        } else {
+                #if "a" is not a matrix, turn it into matrix with number of rows and columns = "rows"
+                x<-matrix(a,rows,rows)
+        }
+        
+        #create function to re-set matrix, either by giving new matrix or numeric vector and number of rows and columns
+        set <- function(y, rows) {
+                if (is.matrix(y)){
+                        x<<-y
+                } else {
+                        x<<-matrix(y,rows,rows)
+                }
+                #set "inv" to NULL, because new matrix was created
+                inv <<- NULL
+        }
+        
+        #create function to retrieve matrix "x"
+        get <- function() x
+        
+        #create function to set "inv" to "inverse"
+        setinv <- function(inverse) inv <<- inverse
+        
+        #create function to retrieve "inv"
+        getinv <- function() inv
+        
+        #return list of above specified functions
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+
+##function calculating the inverse of a matrix and caching its inverse
+##input: x = matrix created with function "makeCacheMatrix"
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        #set "inv" to existing inverse with function getinv
+        inv <- x$getinv()
+        
+        #check whether inverse has been calculated (i.e. is not NULL)
+        if(!is.null(inv)) {
+                #if inverse has been calculated, print message and "inv"
+                message("getting cached data")
+                return(inv)
+        }
+        
+        #if inverse has not been calculated, get matrix
+        data <- x$get()
+        
+        #set "inv" to inverse of matrix by using function solve
+        inv <- solve(data, ...)
+        
+        #cache "inv" with function setinv
+        x$setinv(inv)
+        
+        #print "inv"
+        inv
 }
